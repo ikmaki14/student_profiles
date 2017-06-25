a745823f691d6168b4d93873b1fce23e4226906b
Isabella Maki <ikmaki14@gmail.com>
2017-06-25 20:16:55 +0000

    added page for isabella

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,9 @@
 class StudentsController < ApplicationController
-
+    
+    def isabella
+        @full_name="Isabella Maki"
+        @bio="My birthday is May 14th 2000 and I will be a senior in high school next year."
+        @restaurant="Bien Trucha."
+        @tv_show="Modern Family."
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+    get 'isabella', to: 'students#isabella'
 # Follow this format for your route: get '/harsha', to: 'students#harsha'
 end
